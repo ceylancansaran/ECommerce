c1970e189c6dc0a15f478e06b72a899263b55a0f
ceylan cansaran <ceylan.cansaran@gmail.com>
2020-02-23 15:53:39 +0300

    22.02.2020

---
+++ ECommerce.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Data.Interfaces
+{
+    public interface IUserRepository : IRepository<Entities.User>
+    {
+    }
+}
